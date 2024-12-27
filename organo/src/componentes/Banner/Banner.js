c93d37653deed2168ec23ebc57ed4bb7d842eda0
lucianafs <luciana_ferreiras@outlook.com>
2024-12-27 07:06:14 -0300

    Primeiro commit do projeto Organo

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // Isso é um JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal" />
+        </header>
+    )
+}
